1bdd71648be9ebcf422cdca4d7b0b1d6277dc93f
intanvionasari <vionasari838@gmail.com>
2021-02-17 20:21:42 +0700

    titip save

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,25 @@ class MyApp extends StatelessWidget {
           title: Text("MyApp"),
           backgroundColor: Colors.red
         ),
-        body : Container(
+        body : Column(
+          children:[
+          Container(
           alignment:Alignment.topCenter,
           child : Text(
           'BERITA TERBARU     PERTANDINGAN HARI INI',
           style: TextStyle(fontSize: 15, height :3.0),
+          ),
+        ),
+         Container(
+          margin: const EdgeInsets.all(0.0),
+          padding: const EdgeInsets.all(0.0),
+          decoration: BoxDecoration(
+            border:Border.all(color:Colors.purple)
+            ),
+          child: Image(image: NetworkImage('https://asset.kompas.com/crops/3xTK4p6NP3PqdGM8mE_4Ay1PyRQ=/247x0:958x474/750x500/data/photo/2019/07/27/5d3ba98ca67ed.jpg'),
           )
+        )
+          ]
         )
       ),
     );
